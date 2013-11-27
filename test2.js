f74aff13d15481d0203cce744534af43f9144891
Richard Klancer <rpk@pobox.com>
2013-11-26 23:03:00 -0500

    Quick and dirty, proof-of-concept access logging

--- test2.js
+++ test2.js
@@ -1,6 +1,7 @@
 // Crude first cut creating an object capable of detecting assignments
 !function() {
     'use strict';
+
     function f() {
         x = 10;
         var y = 20;
@@ -11,9 +12,40 @@
         var h = function() {
             y = 2;
         };
-        //g();
+        g();
         console.log("after g(): x is ", x);
         h();
+    }
+
+    function hoist(node) {
+        var name = node.id.name;
+
+        return {
+            "type": "VariableDeclaration",
+            "declarations": [
+                {
+                    "type": "VariableDeclarator",
+                    "id": {
+                        "type": "Identifier",
+                        "name": name
+                    },
+                    "init": {
+                        "type": "FunctionExpression",
+                        "id": {
+                            "type": "Identifier",
+                            "name": name
+                        },
+                        "params": [],
+                        "defaults": [],
+                        "body": node.body,
+                        "rest": null,
+                        "generator": false,
+                        "expression": false
+                    }
+                }
+            ],
+            "kind": "var"
+        };
     }
 
     function isVariable(variable) {
@@ -21,10 +53,7 @@
         if (defs.length > 1) {
             throw new Error("Redefinition of " + variable.name);
         }
-        if (defs.length === 0) {
-            return false;
-        }
-        return defs[0].type === 'Variable';
+        return defs.length > 0 && defs[0].type === 'Variable';
     }
 
     function isFunctionName(variable) {
@@ -32,10 +61,7 @@
         if (defs.length > 1) {
             throw new Error("Redefinition of " + variable.name);
         }
-        if (defs.length === 0) {
-            return false;
-        }        
-        return defs[0].type === 'FunctionName';
+        return defs.length > 0 && defs[0].type === 'FunctionName';
     }
 
     function name(variable) {
@@ -44,18 +70,46 @@
 
     var esprima = require('esprima');
     var escodegen = require('escodegen');
+    var estraverse = require('estraverse');
+
     var escope = require('escope');
 
     var ast = esprima.parse(f.toString());
     var scopes = escope.analyze(ast).scopes;
-    var variables = scopes[0].implicit.variables.map(name);
-    var functionNames = [];
-    scopes.shift();
-    scopes.forEach(function(scope) {
-        variables = variables.concat(scope.variables.filter(isVariable).map(name));
-        functionNames = functionNames.concat(scope.variables.filter(isFunctionName).map(name));
+    var variables = scopes[0].implicit.variables.map(name).concat(scopes[1].variables.filter(isVariable).map(name)).concat('arguments');
+    var functionDeclarations = new Set();
+    var foundVariables = new Set();
+    variables.forEach(foundVariables.add.bind(foundVariables));
+    scopes[1].variables.filter(isFunctionName).forEach(function(functionName) {
+        // could map, and create set from array. But construction from an array doesn't seem to work
+        // in node --harmony yet.
+        functionDeclarations.add(functionName.defs[0].node);
+
+        var actualName = name(functionName);
+        if (foundVariables.has(actualName)) {
+            throw new Error("Redefinition of " + actualName);
+        }
+        variables.push(actualName);
     });
 
+    var hoistedFunctions = [];
+    ast = estraverse.replace(ast, {
+        enter: function(node) {
+            if (node.type && node.type === 'BlockStatement') {
+                var ret = {
+                    type: 'BlockStatement',
+                    body: node.body.filter(function(statement) {
+                        var isFunctionDeclaration = functionDeclarations.has(statement);
+                        if (isFunctionDeclaration) {
+                            hoistedFunctions.push(hoist(statement));
+                        }
+                        return !isFunctionDeclaration;
+                    })
+                };
+                return ret;
+            }
+        }
+    });
 
     var withStatement = {
         type: "Program",
@@ -65,59 +119,30 @@
                 "type": "Identifier",
                 "name": "__context"
             },
-            "body": ast.body[0].body
+            "body": {
+                type: "BlockStatement",
+                body: hoistedFunctions.concat(ast.body[0].body.body)
+            }
         }]
     };
 
-    // Doesn't work. Function bodies are hoisted outside of the with statement, meaning that 
-    // semantics are broken (behavior of the block inside the with statement will be observably
-    // different from behavior of the source function)
-    // What we need to do is "manually" hoist any function declarations to the top of the with
-    // statement. Convert
-    // function f() { ... }
-    // to 
-    // var f = function f() { ... }
-    // and move it up in the source body
-    // - Also be sure to add 'f' to the __context
-    // - Also, just add implicit globals and variables/functions declared in the first child scope
-    //   (skip declarations found in subsequent scopes)
-    // - Also, 'arguments' too!
+    var context = {};
+    var values = {};
 
-    functionNames.forEach(function(functionName) {
-        withStatement.body.unshift({
-            "type": "ExpressionStatement",
-            "expression": {
-                "type": "AssignmentExpression",
-                "operator": "=",
-                "left": {
-                    "type": "MemberExpression",
-                    "computed": true,
-                    "object": {
-                        "type": "Identifier",
-                        "name": "__context"
-                    },
-                    "property": {
-                        "type": "Literal",
-                        "value": functionName,
-                        "raw": "'" + functionName + "'"
-                    }
-                },
-                "right": {
-                    "type": "Identifier",
-                    "name": functionName
-                }
+    variables.forEach(function(variable) {
+        Object.defineProperty(context, variable, {
+            get: function() {
+                console.log("read " + variable);
+                return values[variable];
+            },
+            set: function(value) {
+                console.log("write " + value + " to " + variable);
+                values[variable] = value;
             }
         });
-    });
-
-    var context = {};
-    variables.forEach(function(variable) {
-        context[variable] = undefined;
+        values[variable] = undefined;
     });
 
     var g = new Function('__context', escodegen.generate(withStatement));
-    console.log(g.toString());
     g(context);
-
-    console.log(context);
 }();
