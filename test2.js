34a8c9c7c2bc0de16c79ea6c0dc9092c8ba83938
Richard Klancer <rpk@pobox.com>
2013-11-29 00:40:59 -0500

    Using proxies to track all gets/sets

--- test2.js
+++ test2.js
@@ -1,148 +1,204 @@
+/*global Proxy, Reflect, WeakMap */
 // Crude first cut creating an object capable of detecting assignments
-!function() {
-    'use strict';
+'use strict';
 
-    function f() {
-        x = 10;
-        var y = 20;
-        function g() {
-            x = 1;
-            console.log("g(): x is ", x);
-        }
-        var h = function() {
-            y = 2;
-        };
-        g();
-        console.log("after g(): x is ", x);
-        h();
+require('harmony-reflect');
+var esprima = require('esprima');
+var escodegen = require('escodegen');
+var estraverse = require('estraverse');
+var escope = require('escope');
+
+function f() {
+    var a = {};
+    var b = 0;
+
+    g();
+    g();
+
+    function g() {
+        a[b++] = 0;
+        a[a[0]] = 1;
     }
+}
 
-    function hoist(node) {
-        var name = node.id.name;
+function hoist(node) {
+    var name = node.id.name;
 
-        return {
-            "type": "VariableDeclaration",
-            "declarations": [
-                {
-                    "type": "VariableDeclarator",
+    return {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": name
+                },
+                "init": {
+                    "type": "FunctionExpression",
                     "id": {
                         "type": "Identifier",
                         "name": name
                     },
-                    "init": {
-                        "type": "FunctionExpression",
-                        "id": {
-                            "type": "Identifier",
-                            "name": name
-                        },
-                        "params": [],
-                        "defaults": [],
-                        "body": node.body,
-                        "rest": null,
-                        "generator": false,
-                        "expression": false
-                    }
+                    "params": [],
+                    "defaults": [],
+                    "body": node.body,
+                    "rest": null,
+                    "generator": false,
+                    "expression": false
                 }
-            ],
-            "kind": "var"
-        };
-    }
-
-    function isVariable(variable) {
-        var defs = variable.defs;
-        if (defs.length > 1) {
-            throw new Error("Redefinition of " + variable.name);
-        }
-        return defs.length > 0 && defs[0].type === 'Variable';
-    }
-
-    function isFunctionName(variable) {
-        var defs = variable.defs;
-        if (defs.length > 1) {
-            throw new Error("Redefinition of " + variable.name);
-        }
-        return defs.length > 0 && defs[0].type === 'FunctionName';
-    }
-
-    function name(variable) {
-        return variable.name;
-    }
-
-    var esprima = require('esprima');
-    var escodegen = require('escodegen');
-    var estraverse = require('estraverse');
-
-    var escope = require('escope');
-
-    var ast = esprima.parse(f.toString());
-    var scopes = escope.analyze(ast).scopes;
-    var variables = scopes[0].implicit.variables.map(name).concat(scopes[1].variables.filter(isVariable).map(name)).concat('arguments');
-    var functionDeclarations = new Set();
-    var foundVariables = new Set();
-    variables.forEach(foundVariables.add.bind(foundVariables));
-    scopes[1].variables.filter(isFunctionName).forEach(function(functionName) {
-        // could map, and create set from array. But construction from an array doesn't seem to work
-        // in node --harmony yet.
-        functionDeclarations.add(functionName.defs[0].node);
-
-        var actualName = name(functionName);
-        if (foundVariables.has(actualName)) {
-            throw new Error("Redefinition of " + actualName);
-        }
-        variables.push(actualName);
-    });
-
-    var hoistedFunctions = [];
-    ast = estraverse.replace(ast, {
-        enter: function(node) {
-            if (node.type && node.type === 'BlockStatement') {
-                var ret = {
-                    type: 'BlockStatement',
-                    body: node.body.filter(function(statement) {
-                        var isFunctionDeclaration = functionDeclarations.has(statement);
-                        if (isFunctionDeclaration) {
-                            hoistedFunctions.push(hoist(statement));
-                        }
-                        return !isFunctionDeclaration;
-                    })
-                };
-                return ret;
             }
-        }
-    });
-
-    var withStatement = {
-        type: "Program",
-        body: [{
-            "type": "WithStatement",
-            "object": {
-                "type": "Identifier",
-                "name": "__context"
-            },
-            "body": {
-                type: "BlockStatement",
-                body: hoistedFunctions.concat(ast.body[0].body.body)
-            }
-        }]
+        ],
+        "kind": "var"
     };
+}
 
-    var context = {};
-    var values = {};
+function isVariable(variable) {
+    var defs = variable.defs;
+    if (defs.length > 1) {
+        throw new Error("Redefinition of " + variable.name);
+    }
+    return defs.length > 0 && defs[0].type === 'Variable';
+}
 
-    variables.forEach(function(variable) {
-        Object.defineProperty(context, variable, {
-            get: function() {
-                console.log("read " + variable);
-                return values[variable];
+function isFunctionName(variable) {
+    var defs = variable.defs;
+    if (defs.length > 1) {
+        throw new Error("Redefinition of " + variable.name);
+    }
+    return defs.length > 0 && defs[0].type === 'FunctionName';
+}
+
+function name(variable) {
+    return variable.name;
+}
+
+
+
+var ast = esprima.parse(f.toString());
+var scopes = escope.analyze(ast).scopes;
+var variables = scopes[0].implicit.variables.map(name).concat(scopes[1].variables.filter(isVariable).map(name)).concat('arguments');
+var functionDeclarations = new Set();
+var foundVariables = new Set();
+variables.forEach(foundVariables.add.bind(foundVariables));
+scopes[1].variables.filter(isFunctionName).forEach(function(functionName) {
+    // could map, and create set from array. But construction from an array doesn't seem to work
+    // in node --harmony yet.
+    functionDeclarations.add(functionName.defs[0].node);
+
+    var actualName = name(functionName);
+    if (foundVariables.has(actualName)) {
+        throw new Error("Redefinition of " + actualName);
+    }
+    variables.push(actualName);
+});
+
+var hoistedFunctions = [];
+ast = estraverse.replace(ast, {
+    enter: function(node) {
+        if (node.type && node.type === 'BlockStatement') {
+            var ret = {
+                type: 'BlockStatement',
+                body: node.body.filter(function(statement) {
+                    var isFunctionDeclaration = functionDeclarations.has(statement);
+                    if (isFunctionDeclaration) {
+                        hoistedFunctions.push(hoist(statement));
+                    }
+                    return !isFunctionDeclaration;
+                })
+            };
+            return ret;
+        }
+    }
+});
+
+var withStatement = {
+    type: "Program",
+    body: [{
+        "type": "WithStatement",
+        "object": {
+            "type": "Identifier",
+            "name": "__context"
+        },
+        "body": {
+            type: "BlockStatement",
+            body: hoistedFunctions.concat(ast.body[0].body.body)
+        }
+    }]
+};
+
+function spy(trap, target, name, value) {
+    var printableName, printableValue;
+
+    if (target === context) {
+        printableName = name;
+    } else {
+        printableName = "(object)['" + name + "']";
+    }
+
+    if (trap === 'get') {
+        console.log("get " + printableName);
+    } else if (trap === 'set') {
+        if (typeof value === 'function') {
+            printableValue = "(function)";
+        } else if (typeof value === 'object' && value !== null) {
+            printableValue = "(object)";
+        } else {
+            printableValue = value;
+        }
+
+        console.log("set " + printableName + " to " + printableValue);
+    }
+}
+
+
+function isPrimitive(value) {
+    return (typeof value !== 'function' && typeof value !== 'object') || value === null;
+}
+
+var wrap = (function() {
+    var proxiedValues = new WeakMap();
+
+    return function(obj) {
+        // Don't wrap primitive values
+        if (isPrimitive(obj)) {
+            return obj;
+        }
+
+        return new Proxy(obj, {
+            get: function(target, name) {
+                spy('get', target, name);
+                return Reflect.get(target, name);
             },
-            set: function(value) {
-                console.log("write " + value + " to " + variable);
-                values[variable] = value;
+
+            set: function(target, name, value) {
+                // only wrap objects
+                if (!isPrimitive(value) && !proxiedValues.has(value)) {
+                    value = proxiedValues[value] = wrap(value);
+                }
+                spy('set', target, name, value);
+                Reflect.set(target, name, value);
             }
         });
-        values[variable] = undefined;
-    });
+    };
+}());
 
-    var g = new Function('__context', escodegen.generate(withStatement));
-    g(context);
-}();
+var context = {};
+var values = {};
+
+// Can't use proxy wrapper for the with-context because with doesn't work properly with proxies
+variables.forEach(function(variable) {
+    Object.defineProperty(context, variable, {
+        get: function() {
+            spy('get', context, variable);
+            return values[variable];
+        },
+        set: function(value) {
+            spy('set', context, variable, value);
+            values[variable] = wrap(value);
+        }
+    });
+    values[variable] = undefined;
+});
+
+var g = new Function('__context', escodegen.generate(withStatement));
+g(context);
